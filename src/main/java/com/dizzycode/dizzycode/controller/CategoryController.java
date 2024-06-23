96aeabadbaf4420d25420f6a285507bee3419422
YonghwaJeong <yonghwa814@naver.com>
2024-06-23 21:16:33 +0900

    [feat] Category rework

---
+++ src/main/java/com/dizzycode/dizzycode/controller/CategoryController.java
@@ -0,0 +1,35 @@
+package com.dizzycode.dizzycode.controller;
+
+import com.dizzycode.dizzycode.dto.category.CategoryCreateDTO;
+import com.dizzycode.dizzycode.dto.category.CategoryDetailDTO;
+import com.dizzycode.dizzycode.service.CategoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class CategoryController {
+
+    private final CategoryService categoryService;
+
+    @PostMapping("/rooms/{roomId}/categories")
+    public ResponseEntity<CategoryDetailDTO> createCategory(@RequestBody CategoryCreateDTO categoryCreateDTO, @PathVariable Long roomId) {
+
+        return new ResponseEntity<>(categoryService.createCategory(roomId, categoryCreateDTO), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/rooms/{roomId}/categories")
+    public ResponseEntity<List<CategoryDetailDTO>> categoryList(@PathVariable Long roomId) {
+
+        return new ResponseEntity<>(categoryService.categoryList(roomId), HttpStatus.OK);
+    }
+
+    @GetMapping("/rooms/{roomId}/channels/{categoryId}")
+    public ResponseEntity<CategoryDetailDTO> categoryRetrieve(@PathVariable Long roomId, @PathVariable Long categoryId) {
+
+        return new ResponseEntity<>(categoryService.categoryRetrieve(roomId, categoryId), HttpStatus.OK);
+    }
+}
--- src/main/java/com/dizzycode/dizzycode/repository/CategoryRepository.java
+++ src/main/java/com/dizzycode/dizzycode/repository/CategoryRepository.java
@@ -1,7 +1,6 @@
 package com.dizzycode.dizzycode.repository;
 
 import com.dizzycode.dizzycode.domain.Category;
-import com.dizzycode.dizzycode.domain.Channel;
 import com.dizzycode.dizzycode.domain.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -9,7 +8,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface ChannelRepository extends JpaRepository<Channel, Long> {
-    List<Channel> findChannelsByCategory(Category category);
-    Channel findChannelByChannelId(Long channelId);
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+    public List<Category> findCategoriesByRoom(Room room);
+    public Category findCategoryByCategoryId(Long categoryId);
 }
