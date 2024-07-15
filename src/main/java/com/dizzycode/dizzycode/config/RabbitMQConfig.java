aa6152e8a251fa4f9b6b762e7394213d97029998
hbam1 <hbbaamm@gmail.com>
2024-07-15 14:43:36 +0900

    [fix] rabbitMQ host

--- src/main/java/com/dizzycode/dizzycode/config/RabbitMQConfig.java
+++ src/main/java/com/dizzycode/dizzycode/config/RabbitMQConfig.java
@@ -10,6 +10,7 @@ import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
 import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -19,6 +20,8 @@ public class RabbitMQConfig {
     private static final String CHAT_QUEUE_NAME = "chat.queue";
     private static final String CHAT_EXCHANGE_NAME = "chat.exchange";
     private static final String ROUTING_KEY = "rooms.*";
+    @Value("${spring.rabbitmq.host}")
+    private String rabbitmqHost;
 
     //Queue 등록
     @Bean
@@ -52,7 +55,7 @@ public class RabbitMQConfig {
     @Bean
     public ConnectionFactory connectionFactory() {
         CachingConnectionFactory factory = new CachingConnectionFactory();
-        factory.setHost("localhost");
+        factory.setHost(rabbitmqHost);
         factory.setPort(5672);
         factory.setVirtualHost("/");
         factory.setUsername("guest");
